9e958baeb9e2bc28dcbd442d2caa61a05f0866af
Andrew Yongbin Kim <91082935+yongbin4@users.noreply.github.com>
2023-07-07 09:52:16 -0600

    Time: 65 ms (28.71%), Space: 41.8 MB (80.60%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -18,10 +18,12 @@ var merge = function(nums1, m, nums2, n) {
     let k = m + n - 1;
     
     while (j >= 0) {
-        // if the last value of the 
+        // if the last value of the nums1 is greater than a last value of the nums2
         if (i >= 0 && nums1[i] > nums2[j]) {
+            // put the last value of the nums1 to the m + n - 1;
             nums1[k--] = nums1[i--];
         } else {
+            // else last value of the nums2 to the m + n - 1;
             nums1[k--] = nums2[j--];
         }
     }
