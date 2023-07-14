8bc2d5e87d8d8d554ad41f4314daee1850613819
Andrew Yongbin Kim <91082935+yongbin4@users.noreply.github.com>
2023-07-13 23:59:25 -0600

    Time: 61 ms (94.59%), Space: 44.8 MB (47.39%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -5,10 +5,14 @@
 var removeDuplicates = function(nums) {
     let j = 1;
     for (let i = 1; i < nums.length; i++){
+        // if the lower index is not equal to higher index
         if (nums[i] != nums[j-1]){
+            // then lower index is higher index value
             nums[j] = nums[i]
+            // increment lower index
             j++
         }
+        // else increment higher index 
     }
     return j
 };
