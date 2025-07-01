1ed5a0ca5529ec31af3ecc511830c767395279ff
Andrew Yongbin Kim <yongbin2@ualberta.ca>
2025-07-01 01:51:41 -0600

    Time: 8 ms (79.41%), Space: 19.2 MB (68.9%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,69 @@
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+#         boundary = self.boundary(height)
+#         print(boundary)
+#         min_height = 0
+#         water = 0
+#         for i in range(0,len(boundary), 2):
+#             min_height = min(height[boundary[i]],height[boundary[i+1]])
+#             for j in range(boundary[i], boundary[i+1]):
+#                 if j == 0:
+#                     pass
+#                 else:
+#                     water += height[boundary[i]] - height[j]
+#         return water
+
+#     def boundary(self, height: List[int]) -> List[int]:
+#         boundary = []
+#         i = 0
+#         while i < len(height) - 1:
+#             for j in range(i+1,len(height)):
+#                 if height[i] > height[j]:
+#                     if j == len(height) - 1:
+#                         i += 1
+#                     else:
+#                         pass
+#                 else:
+#                     if i == 0 and height[i] == 0:
+#                         i = j
+#                         break
+#                     else:
+#                         pass
+#                     boundary.append(i)
+#                     i = j
+#                     boundary.append(j)
+#                     break    
+#         return boundary   
+
+# if the first block and the last block is the same size the next bllck that is less must create water
+# 
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        start = 0
+        end = len(height) - 1
+        water = 0
+        
+        while start < end:
+            if height[start] <= height[end]:
+                # If start is lower or equal, process from start side
+                current_height = height[start]
+                start += 1
+                # Add water for all positions lower than current_height
+                while start < end and height[start] < current_height:
+                    water += current_height - height[start]
+                    start += 1
+            else:
+                # If end is lower, process from end side
+                current_height = height[end]
+                end -= 1
+                # Add water for all positions lower than current_height
+                while start < end and height[end] < current_height:
+                    water += current_height - height[end]
+                    end -= 1
+        
+        return water
+
+
+            
+                
