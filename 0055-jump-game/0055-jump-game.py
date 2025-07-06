2adb14f3311a674c53ad027e9cae84c9302ca721
Andrew Yongbin Kim <yongbin2@ualberta.ca>
2025-07-06 17:43:08 -0600

    Time: 6994 ms (5.01%), Space: 13.7 MB (6.53%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,20 +1,29 @@
-from typing import List
-
-class Solution:
+class Solution(object):
     def canJump(self, nums):
-        max_current = 0
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # [2,3,1,1,4]
+        # [T,T,T,T,T]
         
-        if len(nums) == 0 or len(nums) == 1:
-            return True
-
-        for position in range(len(nums)):
-
-            if position > max_current:
+        # [3,2,1,0,4]
+        # [T,Tj,i,T,F]
+        n = len(nums)
+        dp = [False] * n
+        dp[0] = True
+        for i in range(1, n):
+            for j in range(i, -1, -1):
+                if dp[j] == True:
+                    gap = i-j
+                    if nums[j] >= gap:
+                        dp[i] = True
+                        break
+                else:
+                    continue
+            if dp[i] == False:
                 return False
 
-            max_current = max(max_current, position + nums[position])
-            if max_current >= len(nums) - 1:
-                return True
+        return dp[-1]
         
-        return False
         
