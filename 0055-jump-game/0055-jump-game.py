6b0c18cebab65983b590572cc699ad3053a78116
Andrew Yongbin Kim <yongbin2@ualberta.ca>
2025-07-07 16:42:24 -0600

    Time: 9028 ms (5%), Space: 13.8 MB (6.5%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -4,37 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # [2,3,1,1,4]
-        # [T,T,T,T,T]
-        # [2,3,1,1,4]
-        # [0,1,2,3,4]
+        n = len(nums)
+        dp = [False] * n
+        dp[0] = True
         
-        # [3,2,1,0,4]
-        # [T,Tj,i,T,F]
-        if len(nums) == 0 or len(nums) == 1:
-            return True
+        for i in range(1, n):
+            for j in range(i-1, -1, -1):  # Start from i-1, not i
+                if dp[j] and nums[j] >= (i - j):
+                    dp[i] = True
+                    break
         
-        max_current = 0
-        for i in range(len(nums)):
-            if i > max_current:
-                return False
-            max_current = max(max_current, nums[i] + i)
-            if max_current >= len(nums) - 1:
-                return True
-        return False
-            
-        
-        # n = len(nums)
-        # dp = [False] * n
-        # dp[0] = True
-        # for i in range(1, n):
-        #     for j in range(i, -1, -1):
-        #         if dp[j] == True:
-        #             gap = i-j
-        #             if nums[j] >= gap:
-        #                 dp[i] = True
-        #                 break
-        #     if dp[i] == False:
-        #         return False
-
-        # return dp[-1]
+        return dp[-1]  # Only check the last position
