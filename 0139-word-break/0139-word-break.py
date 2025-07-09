c45b1b3950625586a5f82e4ebf506b7a357a7eda
Andrew Yongbin Kim <yongbin2@ualberta.ca>
2025-07-09 03:10:41 -0600

    Time: 192 ms (5.02%), Space: 12.5 MB (69.64%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        n = len(s)
+        dp = [False] * (n+1)
+        dp[0] = True
+        
+        for i in range(1, n+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    print(dp)
+                    break
+
+        return dp[n]
