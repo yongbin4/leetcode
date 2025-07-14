4135c2cdd38b5aa419356bd5aedd6e3ad76f99fc
Andrew Yongbin Kim <yongbin2@ualberta.ca>
2025-07-14 01:04:31 -0600

    Time: 27 ms (71.2%), Space: 12.8 MB (15.31%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -6,20 +6,38 @@ class Solution(object):
         :rtype: bool
         """
 
-        position = set()
-        len_ransom = len(ransomNote)
+        # position = set()
+        # len_ransom = len(ransomNote)
 
-        for char in ransomNote:
-            for j in range(len(magazine)):
-                if char == magazine[j] and (j not in position):
-                    print(char, magazine[j])
-                    position.add(j)
-                    if len(position) == len_ransom:
-                        return True
-                    else:
-                        break
+        # for char in ransomNote:
+        #     for j in range(len(magazine)):
+        #         if char == magazine[j] and (j not in position):
+        #             print(char, magazine[j])
+        #             position.add(j)
+        #             if len(position) == len_ransom:
+        #                 return True
+        #             else:
+        #                 break
 
-        return True if len(position) == len(ransomNote) else False
+        # return True if len(position) == len(ransomNote) else False
+
+        counter = {}
+
+        for c in magazine:
+            if c not in counter:
+                counter[c] = 1
+            else:
+                counter[c] += 1
+
+        for c in ransomNote:
+            if c in counter and counter[c] == 1:
+                del counter[c]
+            elif c in counter:
+                counter[c] -= 1
+            else:
+                return False
+        
+        return True
                     
 
         
