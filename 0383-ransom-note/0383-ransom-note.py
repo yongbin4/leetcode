15d39f857f339e1354b7b9e4e6fc27f34c59c708
Andrew Yongbin Kim <yongbin2@ualberta.ca>
2025-07-14 00:51:31 -0600

    Time: 1161 ms (5.28%), Space: 13.6 MB (5.09%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,25 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        result = False
-        for letter in ransomNote:
-            if letter in magazine:
-                result = True
-                magazine = magazine.replace(letter, "", 1)
-            else:
-                result = False
-                return result
-        return result
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+
+        position = set()
+        len_ransom = len(ransomNote)
+
+        for char in ransomNote:
+            for j in range(len(magazine)):
+                if char == magazine[j] and (j not in position):
+                    print(char, magazine[j])
+                    position.add(j)
+                    if len(position) == len_ransom:
+                        return True
+                    else:
+                        break
+
+        return True if len(position) == len(ransomNote) else False
+                    
+
+        
