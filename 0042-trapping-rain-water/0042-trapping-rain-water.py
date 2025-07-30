07acb5b218143891436ff85ca6a8c718b25b5c66
Andrew Yongbin Kim <yongbin2@ualberta.ca>
2025-07-30 14:22:50 -0600

    Time: 7 ms (88.2%), Space: 13.6 MB (63.57%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,36 +1,23 @@
 class Solution(object):
     def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        # track highest bar from left and right
-        n = len(height)
-        left_height, right_height = [0] * n, [0] * n
-        for i in range(n):
-            if i == 0:
-                left_height[i] = height[i]
+        # start from the either end
+        # when there is a same height each end the it counts any height lower than that
+
+        left = 0
+        right = len(height) - 1
+        water = 0
+
+        while left < right:
+            if height[left] <= height[right]:
+                height_point = height[left]
+                left += 1
+                while height[left] < height_point:
+                    water += height_point - height[left]
+                    left += 1
             else:
-                left_height[i] = max(height[i], left_height[i-1])
-        
-        for i in range(n-1, -1, -1):
-            if i == len(height)-1:
-                right_height[i] = height[i]
-            else:
-                right_height[i] = max(height[i], right_height[i+1])
-        
-        # height =     [0,1,0,2,1,0,1,3,2,1,2,1]
-        # left_height= [0,1,1,2,2,2,2,3,3,3,3,3]
-        #right_height= [3,3,3,3,3,3,3,3,2,2,2,1]
-        #smaller_height[0,1,1,2,2,2,2,3,2,2,2,1]
-        smaller_height = []
-        for i in range(n):
-            smaller_height.append(min(left_height[i], right_height[i]))
-        
-        result = 0
-        for i in range(n):
-            result += smaller_height[i] - height[i]
-            
-        return result
-        
-        
+                height_point = height[right]
+                right -= 1
+                while height[right] < height_point:
+                    water += height_point - height[right]
+                    right -= 1
+        return water
