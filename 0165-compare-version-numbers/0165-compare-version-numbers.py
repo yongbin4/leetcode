b0f8531c0e463fd4d27b0a9c289be56f21252b08
Andrew Yongbin Kim <yongbin2@ualberta.ca>
2025-09-07 17:10:57 -0600

    Time: 4 ms (8.9%), Space: 12.6 MB (12.78%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -5,25 +5,26 @@ class Solution(object):
         :type version2: str
         :rtype: int
         """
+
         # find integeters separated by dots so 1.2 has two part 1 and 2 1.001 | 1 and 1
         version1_list = version1.split(".")
         version2_list = version2.split(".")
-        max_length = max(len(version1_list), len(version2_list))
-        if len(version1_list) > len(version2_list):
-            diff = len(version1_list) - len(version2_list)
-            for i in range(diff):
-                version2_list.append("0")
-        else:
-            diff = len(version2_list) - len(version1_list)
-            for i in range(diff):
-                version1_list.append("0")
+        
+        min_length = min(len(version1_list), len(version2_list))
                 
-        for i in range(max_length):
+        for i in range(min_length):
             if int(version1_list[i]) > int(version2_list[i]):
                 return 1
             elif int(version2_list[i]) > int(version1_list[i]):
                 return -1
-            elif int(version2_list[i]) == int(version1_list[i]):
-                print(int(version2_list[i]) == int(version1_list[i]))
-                continue
+
+        if len(version1_list) > len(version2_list):
+            for i in range(min_length, len(version1_list)):
+                if int(version1_list[i]) > 0:
+                    return 1
+        else:
+            for i in range(min_length, len(version2_list)):
+                if int(version2_list[i]) > 0:
+                    return -1
+
         return 0
